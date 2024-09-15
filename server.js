f96d602a277fc045c6e86a1942598fe08ea03e68
Harrisonk76 <165740611+Harrisonk76@users.noreply.github.com>
2024-09-15 04:04:34 -0700

    update

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Middleware
 app.use(bodyParser.json());
 
-// Database Connection
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/plants", plantRoutes);
 app.use("/api/care-tips", careTipRoutes);
